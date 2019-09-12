b9ef006fdf0456770e365e0b5bb6822c3f77c09c
andrew stehno <andrew_stehno@yahoo.com>
2019-09-11 23:26:51 -0600

    updated functions for hide button, and display all

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,12 +4,11 @@ $(document).ready(function () {
     // ===============================================
 
     let gameData = {
-        startButton: document.getElementById("#start").innerHTML,
         counter: 15,
         userAnswer: "",
         correct: 0,
         incorrect: 0,
-    }
+    };
 
     let q1 = {
         question: '"Wurst" and "Worst", are examples of what?',
@@ -19,7 +18,7 @@ $(document).ready(function () {
         wrong3: 'Cinnamins',
         homonyms: true,
         giphy: undefined,
-    }
+    };
 
     // Game Functions:
     // ===============================================
@@ -31,53 +30,54 @@ $(document).ready(function () {
     function init() {
         hideButton();
         displayAll();
-        countDown();
+        // countDown();
     }
 
     function hideButton() {
-        gameData.startButton.style.dislay = "none";
+        document.getElementById("start").style.visibility = "hidden";
     };
 
     function displayAll() {
-
+        $("#question").html(q1.question);
+        $("#choices").append(<input type="radio" name="q1">q1.answer</input>);
     };
-// let timer = setInterval() {}
-    function countDown() {
-        let timer = setInterval(function() {
-            counter--
-            if (q1.homonyms === true) {
-                rightAnswer();
-            }
-            ele if (counter === 0) {
-                console.log("oops");
-                clearInterval(timer);
-                wrongAnswer();
-            }
-        }, 1000);
-    };
+// // let timer = setInterval() {}
+//     function countDown() {
+//         let timer = setInterval(function() {
+//             counter--
+//             if (q1.homonyms === true) {
+//                 rightAnswer();
+//             }
+//             ele if (counter === 0) {
+//                 console.log("oops");
+//                 clearInterval(timer);
+//                 wrongAnswer();
+//             }
+//         }, 1000);
+//     };
 
-    function rightAnswer() {
+//     function rightAnswer() {
 
-    };
+//     };
 
-    function wrongAnswer() {
+//     function wrongAnswer() {
 
-    };
+//     };
 
-    function clockReset() {
+//     function clockReset() {
 
-    };
+//     };
 
-    function displayButton(); {
+    // function displayButton() {
+    //     document.getElementById("start").style.display;
+    // };
 
-    };
+//     function gameResults() {
 
-    function gameResults() {
+//     };
 
-    };
+//     function gameReset() {
 
-    function gameReset() {
-
-    };
+//     };
 
 });
