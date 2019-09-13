2113e3598c457bf5fa3dcfea152dd0a57d1ef2a0
andrew stehno <andrew_stehno@yahoo.com>
2019-09-12 23:05:11 -0600

    commenting out and adding new options for displaying questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,18 +7,22 @@ $(document).ready(function () {
         counter: 15,
         userAnswer: "",
         correct: 0,
-        incorrect: 0,
+        incorrect: 0
     };
 
     let q1 = {
         question: '"Wurst" and "Worst", are examples of what?',
-        answer: 'Homonyms',
-        wrong1: 'Synonyms',
-        wrong2: 'Heteronyms',
-        wrong3: 'Cinnamins',
-        homonyms: true,
-        giphy: undefined,
+        choices: ['Homonyms', 'Synonyms', 'Heteronyms', 'Cinnamins'],
+        correct: q1.choices[0],
+        giphy: undefined
     };
+
+    let q2 = {
+        question: 'German sausages are known as?',
+        choices: ['Lil Smokies', 'Hot Dogs', 'Bratwurst', 'J-Dawgs'],
+        correct: q2.choices[2],
+        giphy: undefined
+    }
 
     // Game Functions:
     // ===============================================
@@ -29,7 +33,7 @@ $(document).ready(function () {
 
     function init() {
         hideButton();
-        displayAll();
+        display();
         // countDown();
     }
 
@@ -37,47 +41,71 @@ $(document).ready(function () {
         document.getElementById("start").style.visibility = "hidden";
     };
 
-    function displayAll() {
-        $("#question").html(q1.question);
-        $("#choices").append(<input type="radio" name="q1">q1.answer</input>);
-    };
-// // let timer = setInterval() {}
-//     function countDown() {
-//         let timer = setInterval(function() {
-//             counter--
-//             if (q1.homonyms === true) {
-//                 rightAnswer();
-//             }
-//             ele if (counter === 0) {
-//                 console.log("oops");
-//                 clearInterval(timer);
-//                 wrongAnswer();
-//             }
-//         }, 1000);
-//     };
+    function display(q1) {
+        $("#question").html(globalThis.question);
+        let userGuess = "";
+        for (let i = 0; i < 4; i++) {
+            let userChoice = $('<div>');
+            let choice1 = globalThis.choice1[i];
+            let choice2 = globalThis.choice1[i];
+            let choice3 = globalThis.choice1[i];
+            let choice4 = globalThis.choice1[i];
 
-//     function rightAnswer() {
+        }console.log(choice1);
 
-//     };
+    }display(q1);
 
-//     function wrongAnswer() {
+    // function displayAll() {
+    //     $("#question").html(q1.question);
+    //     function mulChoic() {
+    //         let radioBtn = $('<input type="radio" name="q1"/>');
+    //         $('#choices').append(q1.answer);
+    //         $('#choices').append(q1.wrong1);
+    //         $('#choices').append(q1.wrong2);
+    //         $('#choices').append(q1.wrong3);
+    //         radioBtn.prependTo('#choices');
+    //     }
+    //     mulChoic();
+    // };
 
-//     };
+    // let timer = setInterval(){}
+    // function countDown() {
+    //     let timer = setInterval(function () {
+    //         counter--
+    //         if (q1.homonyms === true) {
+    //             rightAnswer();
+    //         }
+    //         else if (counter === 0) {
+    //             console.log("oops");
+    //             clearInterval(timer);
+    //             wrongAnswer();
+    //         }
+    //     }, 1000);
+    // }
 
-//     function clockReset() {
 
-//     };
+    //     function rightAnswer() {
+
+    //     };
+
+    //     function wrongAnswer() {
+
+    //     };
+
+    //     function clockReset() {
+
+    //     };
 
     // function displayButton() {
     //     document.getElementById("start").style.display;
     // };
 
-//     function gameResults() {
+    //     function gameResults() {
 
-//     };
+    //     };
 
-//     function gameReset() {
+    //     function gameReset() {
 
-//     };
+    //     };
 
 });
