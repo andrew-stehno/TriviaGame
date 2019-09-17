a7f6d5d5cf8c3cac9d716e9702071af4900bcff6
andrew stehno <andrew_stehno@yahoo.com>
2019-09-17 00:55:38 -0600

    per TA, code was revamped to make less distracting.  timer function was renamed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,135 +10,56 @@ $(document).ready(function () {
         choice3: '<button type="button" class="btn btn-success">Heteronyms</button>',
         choice4: '<button type="button" class="btn btn-success">Cinnamins</button>',
         statement1: 'Nice shot Commander!',
-        statement2: "Phonix is not your stront suite!",
+        statement2: "Phonics is not your stront suite!",
         answer: 'Homonyms',
         giphy1: '<iframe src="https://giphy.com/embed/J6Nj4TzJWtyWk" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/star-trek-lasers-sausage-J6Nj4TzJWtyWk">via GIPHY</a></p>',
         giphy2: '<iframe src="https://giphy.com/embed/3o7rbRW09ZmTRPeUtW" width="480" height="400" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/justin-g-dance-sausage-party-3o7rbRW09ZmTRPeUtW">via GIPHY</a></p>',
     };
 
-    const q2 = {
-        question: 'German sausages are known as?',
-        choice1: '<button type="button" class="btn btn-success">Lil Smokies</button>',
-        choice2: '<button type="button" class="btn btn-success">Hot Dogs</button>',
-        choice3: '<button type="button" class="btn btn-success">Bratwurst</button>',
-        choice4: '<button type="button" class="btn btn-success">J-Dawgs</button>',
-        answer: 'Bratwurst',
-        statement1: 'Prost!',
-        statement2: 'Nine Nine Nine Nine Nine!',
-        giphy1: '<iframe src="https://giphy.com/embed/mOOuUUIEEgq6A" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/shakeshack-beer-cheers-steins-mOOuUUIEEgq6A">via GIPHY</a></p>',
-        giphy2: '<iframe src="https://giphy.com/embed/3oEhn4ZRr2rC2rlKmI" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/art-lol-food-3oEhn4ZRr2rC2rlKmI">via GIPHY</a></p>',
-    };
+    // const q2 = {
+    //     question: 'German sausages are known as?',
+    //     choice1: '<button type="button" class="btn btn-success">Lil Smokies</button>',
+    //     choice2: '<button type="button" class="btn btn-success">Hot Dogs</button>',
+    //     choice3: '<button type="button" class="btn btn-success">Bratwurst</button>',
+    //     choice4: '<button type="button" class="btn btn-success">J-Dawgs</button>',
+    //     answer: 'Bratwurst',
+    //     statement1: 'Prost!',
+    //     statement2: 'Nine Nine Nine Nine Nine!',
+    //     giphy1: '<iframe src="https://giphy.com/embed/mOOuUUIEEgq6A" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/shakeshack-beer-cheers-steins-mOOuUUIEEgq6A">via GIPHY</a></p>',
+    //     giphy2: '<iframe src="https://giphy.com/embed/3oEhn4ZRr2rC2rlKmI" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/art-lol-food-3oEhn4ZRr2rC2rlKmI">via GIPHY</a></p>',
+    // };
 
-    const q3 = {
-        question: 'What foods are Star Fleet computers not capable of replicating?',
-        choice1: '<button type="button" class="btn btn-success">Alcohal</button>',
-        choice2: '<button type="button" class="btn btn-success">Hot Dogs</button>',
-        choice3: '<button type="button" class="btn btn-success">Pretzels</button>',
-        choice4: '<button type="button" class="btn btn-success">Nuts</button>',
-        answer: 'Hot Dogs',
-        statement1: 'Mystery meat is far too complex for even the super computers of tomorrow!',
-        statement2: 'Please, bar foods are staples of deep space exploration!',
-        giphy1: '<iframe src="https://giphy.com/embed/vp122eOzO0Hxm" width="480" height="333" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/star-trek-wow-vp122eOzO0Hxm">via GIPHY</a></p>',
-        giphy2: '<iframe src="https://giphy.com/embed/mFOHqYNbjdsZy" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/kirk-mFOHqYNbjdsZy">via GIPHY</a></p>',
-    };
+    // const q3 = {
+    //     question: 'What foods are Star Fleet computers not capable of replicating?',
+    //     choice1: '<button type="button" class="btn btn-success">Alcohal</button>',
+    //     choice2: '<button type="button" class="btn btn-success">Hot Dogs</button>',
+    //     choice3: '<button type="button" class="btn btn-success">Pretzels</button>',
+    //     choice4: '<button type="button" class="btn btn-success">Nuts</button>',
+    //     answer: 'Hot Dogs',
+    //     statement1: 'Mystery meat is far too complex for even the super computers of tomorrow!',
+    //     statement2: 'Please, bar foods are staples of deep space exploration!',
+    //     giphy1: '<iframe src="https://giphy.com/embed/vp122eOzO0Hxm" width="480" height="333" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/star-trek-wow-vp122eOzO0Hxm">via GIPHY</a></p>',
+    //     giphy2: '<iframe src="https://giphy.com/embed/mFOHqYNbjdsZy" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/kirk-mFOHqYNbjdsZy">via GIPHY</a></p>',
+    // };
 
     // const q4 = {
-    //     question: '?',
-    //     choice1: '<button type="button" class="btn btn-success"></button>',
-    //     choice2: '<button type="button" class="btn btn-success"></button>',
-    //     choice3: '<button type="button" class="btn btn-success"></button>',
-    //     choice4: '<button type="button" class="btn btn-success"></button>',
-    //     answer: '',
-    //     statement1: '',
-    //     statement2: '',
-    //     giphy1: '',
-    //     giphy2: '',
+    //     question: 'Hot Dogs contain which of the following items?',
+    //     choice1: '<button type="button" class="btn btn-success">Desert Tortoise</button>',
+    //     choice2: '<button type="button" class="btn btn-success">Beef</button>',
+    //     choice3: '<button type="button" class="btn btn-success">Racoon</button>',
+    //     choice4: '<button type="button" class="btn btn-success">Children</button>',
+    //     answer: 'Beef',
+    //     statement1: 'That may be true, somewhere in the multi-verse...',
+    //     statement2: 'It pays to be good kids!',
+    //     giphy1: '<iframe src="https://giphy.com/embed/2v06skq0mKgCs" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/foxadhd-fox-adhd-2v06skq0mKgCs">via GIPHY</a></p>',
+    //     giphy2: '<iframe src="https://giphy.com/embed/3o7TKO3AC2o5cOkZfG" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cute-kawaii-hotdog-3o7TKO3AC2o5cOkZfG">via GIPHY</a></p>',
     // };
 
-    // const q5 = {
-    //     question: '?',
-    //     choice1: '<button type="button" class="btn btn-success"></button>',
-    //     choice2: '<button type="button" class="btn btn-success"></button>',
-    //     choice3: '<button type="button" class="btn btn-success"></button>',
-    //     choice4: '<button type="button" class="btn btn-success"></button>',
-    //     answer: '',
-    //     statement1: '',
-    //     statement2: '',
-    //     giphy1: '',
-    //     giphy2: '',
-    // };
-
-    // const q6 = {
-    //     question: '?',
-    //     choice1: '<button type="button" class="btn btn-success"></button>',
-    //     choice2: '<button type="button" class="btn btn-success"></button>',
-    //     choice3: '<button type="button" class="btn btn-success"></button>',
-    //     choice4: '<button type="button" class="btn btn-success"></button>',
-    //     answer: '',
-    //     statement1: '',
-    //     statement2: '',
-    //     giphy1: '',
-    //     giphy2: '',
-    // };
-
-    // const q7 = {
-    //     question: '?',
-    //     choice1: '<button type="button" class="btn btn-success"></button>',
-    //     choice2: '<button type="button" class="btn btn-success"></button>',
-    //     choice3: '<button type="button" class="btn btn-success"></button>',
-    //     choice4: '<button type="button" class="btn btn-success"></button>',
-    //     answer: '',
-    //     statement1: '',
-    //     statement2: '',
-    //     giphy1: '',
-    //     giphy2: '',
-    // };
-
-    // const q8 = {
-    //     question: '?',
-    //     choice1: '<button type="button" class="btn btn-success"></button>',
-    //     choice2: '<button type="button" class="btn btn-success"></button>',
-    //     choice3: '<button type="button" class="btn btn-success"></button>',
-    //     choice4: '<button type="button" class="btn btn-success"></button>',
-    //     answer: '',
-    //     statement1: '',
-    //     statement2: '',
-    //     giphy1: '',
-    //     giphy2: '',
-    // };
-
-    // const q9 = {
-    //     question: '?',
-    //     choice1: '<button type="button" class="btn btn-success"></button>',
-    //     choice2: '<button type="button" class="btn btn-success"></button>',
-    //     choice3: '<button type="button" class="btn btn-success"></button>',
-    //     choice4: '<button type="button" class="btn btn-success"></button>',
-    //     answer: '',
-    //     statement1: '',
-    //     statement2: '',
-    //     giphy1: '',
-    //     giphy2: '',
-    // };
-
-    // const q10 = {
-    //     question: '?',
-    //     choice1: '<button type="button" class="btn btn-success"></button>',
-    //     choice2: '<button type="button" class="btn btn-success"></button>',
-    //     choice3: '<button type="button" class="btn btn-success"></button>',
-    //     choice4: '<button type="button" class="btn btn-success"></button>',
-    //     answer: '',
-    //     statement1: '',
-    //     statement2: '',
-    //     giphy1: '',
-    //     giphy2: '',
-    // };
-
-    let questions = [q1, q2, q3];
+    let questions = [q1];
     let qIndex = 0;
     let counter = 11;
     let correct = 0;
     let incorrect = 0;
-    let intervalId;
     let talley = $('<div>');
     let message = $('<div>');
     let gif = $('<div>');
@@ -146,95 +67,77 @@ $(document).ready(function () {
     talley.addClass('data');
     message.addClass('data');
     gif.addClass('data');
-    
+
     // Game Functions:
     // ===============================================
-    // Game Start:
-
-    $("#start").click(function () {
-        clearInterval(intervalId);
-        document.getElementById("start").style.visibility = "hidden";
-        question();
-    });
-
-    // Prompt questions or end game.
-    //================================================
-
-    function question() {
-        if (qIndex === questions.length) {
-            console.log('game over');
-            clearInterval(intervalId);
-            gameOver()
-        } else {
-            // Append data to DOM:====================
-            talley.empty();
-            message.empty();
-            gif.empty();
-            $('.question').empty().append(questions[qIndex].question);
-            $('.choice-1').empty().append(questions[qIndex].choice1);
-            $('.choice-2').empty().append(questions[qIndex].choice2);
-            $('.choice-3').empty().append(questions[qIndex].choice3);
-            $('.choice-4').empty().append(questions[qIndex].choice4);
-            countDown();
-            // Click logs user guess:==================
-            $('button').click(function () {
-                let userGuess = $(this).text();
-                console.log("user guess " + userGuess);
-                // Correct/Incorrect conditionals:=====
-                if (userGuess === questions[qIndex].answer) {
-                    correct++;
-                    console.log("correct: " + correct);
-                    clearInterval(intervalId);
-                    hideMe();
-                    message.append(questions[qIndex].statement1);
-                    gif.append(questions[qIndex].giphy1);
-                    $('.main-body').append(message);
-                    $('.main-body').append(gif);
-                } else {
-                    incorrect++;
-                    console.log(incorrect);
-                    clearInterval(intervalId);
-                    hideMe();
-                    message.append(questions[qIndex].statement2);
-                    gif.append(questions[qIndex].giphy2);
-                    $('.main-body').append(message);
-                    $('.main-body').append(gif);
-                }
-                qIndex++;
-                console.log('array index: ' + qIndex);
-                // Timeout after giphy display:======
-                setTimeout(function () {
-                    message.empty();
-                    gif.empty();
-                    nextQuestion();
-                }, 5000);
-            })
-        }
+    // Main display function:
+    function display() {
+        talley.empty();
+        message.empty();
+        gif.empty();
+        $('.question').empty().append(questions[qIndex].question);
+        $('.choice-1').empty().append(questions[qIndex].choice1);
+        $('.choice-2').empty().append(questions[qIndex].choice2);
+        $('.choice-3').empty().append(questions[qIndex].choice3);
+        $('.choice-4').empty().append(questions[qIndex].choice4);
     };
 
-    // Timer functions:
-    // ==============================================
+    // Timer function:=================================
 
     function countDown() {
-        clearInterval(intervalId);
-        intervalId = setInterval(decrement, 1000);
-    };
-
-    function decrement() {
-        counter--
-        $('.timer').html(counter);
-        if (counter === 0) {
-            incorrect++;
-            qIndex++;
-            clearInterval(intervalId);
-            console.log("incorrect: " + incorrect);
-            swal("Whoa!", "You ran out of time!");
-            nextQuestion();
+        clearInterval(myTimer);
+        setInterval(myTimer, 1000);
+        function myTimer() {
+            counter--;
+            $('.timer').html(counter);
+            if (counter === 0) {
+                clearInterval(myTimer);
+                incorrect++;
+                qIndex++;
+                console.log("timed out incorrect: " + incorrect);
+                // swal("Whoa!", "You ran out of time!");
+                // question();
+            }
         }
     };
 
-    // Supportive functions:
-    // ================================================
+    // function countDown() {
+    //     intervalId = setInterval(decrement, 1000);
+    //     clearInterval(intervalId);
+    // };
+
+    // function decrement() {
+    //     counter--
+    //     $('.timer').html(counter);
+    //     if (counter === 0) {
+    //         incorrect++;
+    //         qIndex++;
+    //         clearInterval(intervalId);
+    //         console.log("timed out incorrect: " + incorrec);
+    //         swal("Whoa!", "You ran out of time!");
+    //         nextQuestion();
+    //     }
+    // };
+
+    // Message display if correct answer function:=====
+
+    function msgCorrect() {
+        message.append(questions[qIndex].statement1);
+        gif.append(questions[qIndex].giphy1);
+        $('.main-body').append(message);
+        // $('.main-body').append(gif);
+    };
+
+    // Message display if wrong answer function:=======
+
+    function msgWrong() {
+        message.append(questions[qIndex].statement2);
+        gif.append(questions[qIndex].giphy2);
+        $('.main-body').append(message);
+        // $('.main-body').append(gif);
+    };
+
+    // Anti display function:==========================
 
     function hideMe() {
         $('.question').empty();
@@ -245,33 +148,84 @@ $(document).ready(function () {
         // $('.timer').empty();
     }
 
-    function nextQuestion() {
-        question();
-    }
+    // Gameover display function:======================
 
     function gameOver() {
-        clearInterval(intervalId);
-        hideMe();;
+        hideMe();
         talley.append("You got " + correct + " correct, and " + incorrect + " wrong!");
         console.log(talley);
         $('.main-body').append(talley);
 
         setTimeout(function () {
-            swal("Game Over!", "You have to start again!"); 
+            swal("Game Over!", "You have to start again!");
         }, 3000);
-        document.onclick = function() {
+
+        document.onclick = function () {
             resetGame();
-          }; 
-    }; 
+        };
+    };
+
+    // Reset game without refresh function:==============
 
     function resetGame() {
-        clearInterval(intervalId);
         qIndex = 0;
         correct = 0;
         incorrect = 0;
         userGuess = null;
         talley.empty();
-        document.getElementById("start").style.visibility ="visible";
+        document.getElementById("start").style.visibility = "visible";
     }
-   
+    //
+    // Game Start:====================================
+    // 
+    $("#start").click(function () {
+        document.getElementById("start").style.visibility = "hidden";
+        question();
+    });
+
+    // Main Process:
+    //================================================
+    // Prompt questions or end game.
+
+    function question() {
+        if (qIndex === questions.length) {
+            console.log('game over');
+            gameOver()
+        } 
+        
+        else {
+            display();
+            countDown();
+            // Click logs user guess==================
+            $('button').click(function () {
+                let userGuess = $(this).text();
+                console.log("user guess " + userGuess);
+
+                if (userGuess === questions[qIndex].answer) {
+                    clearInterval(myTimer);
+                    correct++;
+                    console.log("Correct guess: " + correct);
+                    hideMe();
+                    msgCorrect();
+                }
+
+                else {
+                    incorrect++;
+                    console.log("Incorrect guess " + incorrect);
+                    hideMe();
+                    msgWrong();
+                }
+
+                qIndex++;
+                console.log('array index: ' + qIndex);
+
+                // Timeout after giphy display:=========
+                // setTimeout(function () {
+                //     message.empty();
+                //     gif.empty();
+                //     question();
+                // }, 5000);
+            })
+        }
+    }
 });
